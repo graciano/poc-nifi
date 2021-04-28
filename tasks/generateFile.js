0db071d96c3464813a1dd92dc687eeff83d03356
Matheus Graciano <matheus.andrade@zarpsystem.com.br>
2021-04-28 17:15:14 -0300

    Generate csv file

--- tasks/generateFile.js
+++ tasks/generateFile.js
@@ -1,6 +1,10 @@
 import faker from 'faker';
+import { promises as fs } from 'fs';
 
 const users = [];
+const FILE_NAME = 'users.csv';
+
+const csvUserParse = ({ name, city }) => [name, city].join(',');
 
 [...Array(50)].forEach(() => {
   users.push({
@@ -9,6 +13,8 @@ const users = [];
   })
 });
 
-console.log(users);
-
-export default {}
+(async () => {
+  const csvString = users.map(csvUserParse).join('\r\n');
+  await fs.writeFile(FILE_NAME, csvString);
+  console.log(`${users.length} users in file ${FILE_NAME}`);
+})();
