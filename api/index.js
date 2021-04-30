b3a4b36e708fc96c85f338b1fdde07a82a55fda9
Matheus Graciano <matheus.andrade@zarpsystem.com.br>
2021-04-30 12:06:40 -0300

    Fix api log and use nodemon

--- api/index.js
+++ api/index.js
@@ -2,45 +2,36 @@ const express = require('express')
 const faker = require('faker')
 const { promises: fs } = require('fs')
 
-const LOG_PATH = '/usr/app/api/log/access.log'
+const LOG_PATH = '/usr/app/log/access.log'
 const app = express()
 const port = 3000
 
-function writeLog (...data) {
-  fs.writeFile(LOG_PATH, `[${new Date()}]: ${data.toString()}`)
+app.use(express.json())
+
+function writeLog (str) {
+  fs.appendFile(LOG_PATH, `\r\n[${new Date()}]: ${str}`)
 }
 
 writeLog('initializing app')
 
 app.get('/', (req, res) => {
   writeLog('/')
-  writeLog(req.query)
-  writeLog(req.body)
   res.send('Hello World!')
 })
 
 app.post('/profile', (req, res) => {
   writeLog('post profile')
-  writeLog(req.query)
-  writeLog(req.body)
+  writeLog(`body: ${JSON.stringify(req.body)}`)
   res.send('Profile created!').status(201)
 })
 
 app.get('/profile', (req, res) => {
   writeLog('get profile')
-  writeLog(req.body)
-  writeLog(req.query)
+  writeLog(`query: ${JSON.stringify(req.query)}`)
   res.json({
     ...req.query,
     phone: faker.phone.phoneNumber()
   }).status(201)
-})
-
-app.post('/reward', (req, res) => {
-  writeLog('post reward')
-  writeLog(req.query)
-  writeLog(req.body)
-  res.send('Reward created!').status(201)
 })
 
 app.listen(port, () => {
